8f975d97efb51955832c273c963c5adc47ac880d
Navkaran7007 <nsidhu10@rrc.ca>
2025-11-10 21:35:45 -0600

    Implemented book routes and controller

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import bookRoutes from "./api/v1/routes/bookRoutes";
 
 // Initialize Express application
 const app: Express = express();
@@ -7,5 +8,9 @@ const app: Express = express();
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+app.use(express.json())
+
+app.use("/api/v1", bookRoutes)
+
 
 export default app;
